e2251c45dd8d484c749781e1d599517a848a002c
SummerLee <lixj12@outlook.com>
2017-03-20 09:01:24 +0800

    C# to realize UDP connection

---
+++ Udp.cs
@@ -0,0 +1,29 @@
+```cs
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Udp
+{
+	public partial class Form1: Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		IPAddress ip;
+		IPEndPoint point = new IPEndPoint(IPAddress.Any, 0);
+		string protocol;
+		long time;
+		Socket aimSocket;
+		UdpClient RecUdpClient;
+		
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			cobProtocol.SelectedIndex = 0;
+			txtIP.Text = GetAddressIP();
+			
